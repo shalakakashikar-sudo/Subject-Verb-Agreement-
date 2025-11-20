3bd7a2af66cfe6ec62350dac97b5e578d32b5210
shalakakashikar <shalakakashikar@gmail.com>
2025-11-20 17:13:42 +0000

    re-added trailing slash to base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // CHANGE THIS LINE:
-  base: '/Subject-Verb-Agreement', 
+  base: '/Subject-Verb-Agreement-/', 
   server: {
     port: 3000,
     open: true
