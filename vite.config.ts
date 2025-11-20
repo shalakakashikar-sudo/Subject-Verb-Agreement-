e1a258b0f0c1360e84f8bab67345f9c0d34914a9
shalakakashikar <shalakakashikar@gmail.com>
2025-11-20 17:03:20 +0000

    pushed changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Subject-Verb-Agreement-/', // Matches repository name
+  base: '/', // Matches repository name
   server: {
     port: 3000,
     open: true
